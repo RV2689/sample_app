bbc596fc64aa4d8901680cab39054aa1c11d9b09
RV <andres.herve@yahoo.fr>
2015-02-20 11:09:50 +0100

    Fini avec les actions utilisateurs edit/update, index et destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,12 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:session][:email],
                              params[:session][:password])
     if user.nil?
-      flash.now[:error] = "Combinaison Email/Mot de passe invalide."
+      flash.now[:error] = "Combinaison mail/mot de passe invalide."
       @titre = "S'identifier"
       render 'new'
     else
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
   end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,3 +1,4 @@
+
 module SessionsHelper
 
   def sign_in(user)
@@ -9,6 +10,10 @@ module SessionsHelper
     @current_user = user
   end
 
+  def current_user
+    @current_user ||= user_from_remember_token
+  end
+
   def signed_in?
     !current_user.nil?
   end
@@ -16,6 +21,17 @@ module SessionsHelper
   def sign_out
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+
+
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
   end
 
   private
@@ -26,5 +42,13 @@ module SessionsHelper
 
     def remember_token
       cookies.signed[:remember_token] || [nil, nil]
+    end
+
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
     end
 end
